2eb294427af06d079dcc4e1502990db1b1fce612
NavyNurlyn <navyajrina@gmail.com>
2024-10-02 22:16:27 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -68,7 +68,7 @@ def create_rfm_df(df):
     rfm_df.drop("max_order_timestamp", axis=1, inplace=True)
     
     return rfm_df
-all_df = pd.read_csv("dashboard\main_data.csv")
+all_df = pd.read_csv("dashboard/main_data.csv")
 datetime_columns = ["order_approved_at", "order_delivered_customer_date"]
 all_df.sort_values(by="order_approved_at", inplace=True)
 all_df.reset_index(inplace=True)
